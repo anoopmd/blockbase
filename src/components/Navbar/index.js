4f105ab8ab9a8e9dae32522a7725493f7255709d
Anoop M D <anoop.md1421@gmail.com>
2021-11-12 06:06:07 +0530

    feat: Navbar link highlighting based on active page

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { IconBox } from '@tabler/icons';
+import classnames from 'classnames';
 
 const Navbar = () => {
+  const router = useRouter();
+  const route = router.route;
+
+  const getClassNames = (path) => {
+    return classnames({
+      'font-semibold': route === path
+    });
+  };
+
   return (
     <div className="w-full site-navbar">
       <header className="flex items-center justify-between py-4">
@@ -14,15 +25,15 @@ const Navbar = () => {
             </div>
           </Link>
         </div>
-        <nav className='flex flex-grow justify-end'>
+        <nav className='flex flex-grow justify-end text-sm'>
           <Link href="/accounts">
-            <a className="font-semibold">Accounts</a>
+            <a className={getClassNames('/accounts')}>Accounts</a>
           </Link>
           <Link href="/blocks">
-            <a className="ml-6">Blocks</a>
+            <a className={`ml-6 ${getClassNames('/blocks')}`}>Blocks</a>
           </Link>
           <Link href="/transactions">
-            <a className="ml-6 mr-1">Transactions</a>
+            <a className={`ml-6 mr-1 ${getClassNames('/transactions')}`}>Transactions</a>
           </Link>
         </nav>
       </header>
